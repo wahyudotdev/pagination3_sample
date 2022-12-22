3b2d43c429a11bee172a9e06d7e31e78a00a882f
wahyudotdev <wahyudotdev@gmail.com>
2022-12-22 07:48:13 +0700

    update override method

--- app/src/main/java/com/sample/pagination/ui/ProductViewModel.kt
+++ app/src/main/java/com/sample/pagination/ui/ProductViewModel.kt
@@ -7,26 +7,22 @@ import com.crocodic.core.base.adapter.CorePagingSource
 import com.crocodic.core.base.viewmodel.CoreViewModel
 import com.sample.pagination.data.repository.ProductRepository
 import com.sample.pagination.data.repository.ProductRepositoryImpl
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.first
 
-class ProductViewModel: CoreViewModel() {
-    override fun apiLogout(): Job {
-        TODO("Not yet implemented")
-    }
-
-    override fun apiRenewToken(): Job {
-        TODO("Not yet implemented")
-    }
+class ProductViewModel : CoreViewModel() {
 
     private val repository: ProductRepository = ProductRepositoryImpl()
     private val firstPage = 1
     private val itemPerPage = 10
 
     fun productList() = Pager(CorePagingSource.config(itemPerPage), pagingSourceFactory = {
-        CorePagingSource(firstPage){ page, limit ->
+        CorePagingSource(firstPage) { page, limit ->
             val data = repository.getAllProduct(page, limit).first()
             data // List<Product>
         }
     }).flow.cachedIn(viewModelScope)
+
+    override fun apiLogout() {}
+
+    override fun apiRenewToken() {}
 }
